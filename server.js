72df9a166dd228ea7ec48affa60fe01345884138
babkamen <babkamen@gmail.com>
2019-04-17 20:45:44 +0000

    Add multer

--- server.js
+++ server.js
@@ -2,48 +2,34 @@
 
 var express = require('express');
 var cors = require('cors');
-const formidable = require('formidable'),
-	fs = require('fs'),
-	path = require('path');
-// require and use "multer"...
-
+var multer = require('multer')
+const fs = require('fs')
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.get('/', function(req, res) {
+	res.sendFile(process.cwd() + '/views/index.html');
+});
 
-app.post('/api/fileanalyse', function(req, res) {
-	var form = new formidable.IncomingForm();
-	console.log(req.body);
-	form.parse(req, function(err, fields, files) {
-		console.log("files=", files);
-		if (!files.upfile) {
-			res.status(422).json({
-				error: "No file in request"
-			})
-		}
-		if (err) {
-			res.status(500).json({
-				error: err
-			})
-		}
-		else {
-			res.json({
-        filename:files.upfile.name,
-				size: files.upfile.size
-			})
-		}
+const upload = multer({ dest: 'uploads/' })
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res, next) {
+	console.log(req.file);
+
+	res.json({
+		filename: req.file.originalname,
+		size: req.file.size
 	});
+	fs.unlink(req.file.path);
+
 });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get('/hello', function(req, res) {
+	res.json({ greetings: "Hello, API" });
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.listen(process.env.PORT || 3000, function() {
+	console.log('Node.js listening ...');
 });
